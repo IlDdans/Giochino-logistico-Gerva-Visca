26efbf5e286e02646a296dc7f1182c83c6ed5b29
er cinese visca <viscardijacopo6@gmail.com>
2024-05-18 22:30:02 +0200

    abbiamo aggiunto nuove info alla classe boscaiolo, immagine del boscaiolo, trasformata in un rect e la sua velocità

--- lumberjack.py
+++ lumberjack.py
@@ -1,7 +1,12 @@
 import pygame
-
+from random import randint
 class Boscaiolo:
     def __init__(self, screen, size, pos):
         self.screen=screen
         self.size=size
         self.pos=pos
+
+        self.boscaiolo=pygame.image.load("boscaiolo.png").convert_alpha()
+        self.rect=self.boscaiolo.get_rect
+        self.velocita=[randint(4,8)]
+        
