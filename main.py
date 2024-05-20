d53b1718dc23754ac55532388619d571385d82fb
er cinese visca <viscardijacopo6@gmail.com>
2024-05-20 11:26:51 +0200

    abbiamo messo il boscaiolo sullo schermo

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ pygame.init()
 sfondo=pygame.image.load("foresta.png")
 WIDTH, HEIGHT = 626, 626
 screen=pygame.display.set_mode((WIDTH, HEIGHT))
+boscaiolo=pygame.image.load("boscaiolo.png").convert_alpha()
+boscaiolo = pygame.transform.scale(boscaiolo, (boscaiolo.get_width()*0.4, boscaiolo.get_height() * 0.4))
 pygame.display.set_caption("Lumberjack Quandale Deangle")
 fps=60
 clock=pygame.time.Clock()
@@ -14,6 +16,6 @@ while True:
             pygame.quit()           
             sys.exit()
     screen.blit(sfondo, (0,0))
-    
+    screen.blit(boscaiolo,(WIDTH-550,HEIGHT-250))
     clock.tick(fps)
     pygame.display.flip()
