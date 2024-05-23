bd833abc84a206998f10883d275cdee91b96c850
gervasoni <danielegervasoni838@gmail.com>
2024-05-23 18:53:55 +0200

    faccio cose

--- main.py
+++ main.py
@@ -9,9 +9,18 @@ sfondo=pygame.image.load("foresta.png")
 ramo=pygame.image.load("ramo.png").convert_alpha()
 boscaiolo=pygame.image.load("boscaiolo.png").convert_alpha()
 
+
+boscaiolo = pygame.transform.scale(boscaiolo, (boscaiolo.get_width()*0.3, boscaiolo.get_height() * 0.3))
 larghezzautile=boscaiolo.get_width()
 altezzautile=boscaiolo.get_height()
-boscaiolo = pygame.transform.scale(boscaiolo, (boscaiolo.get_width()*0.3, boscaiolo.get_height() * 0.3))
+
+tronco=pygame.image.load("tronco.jpg").convert_alpha()
+tronco=pygame.transform.scale(tronco, (larghezzautile*0.8, 550))
+tronco_rect=tronco.get_rect(midtop=(WIDTH/2, 0))
+
+boscaiolo_rect=boscaiolo.get_rect(bottomright=(tronco_rect.left, 550))
+
+
 
 pygame.display.set_caption("Lumberjack Quandale Deangle")
 
@@ -26,6 +35,7 @@ while True:
             pygame.quit()           
             sys.exit()
     screen.blit(sfondo, (0,0))
-    screen.blit(boscaiolo,(WIDTH-480,HEIGHT-180))
+    screen.blit(boscaiolo, boscaiolo_rect)
+    screen.blit(tronco, tronco_rect)
     clock.tick(fps)
     pygame.display.flip()
---
+++ sezione.py
@@ -0,0 +1,6 @@
+import pygame
+from random import randint
+
+class Sezione:  
+    def __init__(self, ) -> None:
+        pass
