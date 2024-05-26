c01b555e2ea6d1784529e8eb4cb18c9a879f02de
er cinese visca <viscardijacopo6@gmail.com>
2024-05-26 20:54:52 +0200

    ci siamo quasi

--- main.py
+++ main.py
@@ -20,13 +20,11 @@ boscaiolo=Boscaiolo(screen)
 supporto=[i for i in range(0,9)]
 stati=[]
 rami=[]
-stati.append(None)
 stati.append(1)
 stati.append(rand([0,2]))
-while len(stati)<9:
-    supporto=[i for i in range(0,9)]
+while len(stati)<10:
     n=rand(supporto)
-    if n<8:
+    if n<7:
         stato=stati[-1]
     else:
         if stati[-1]==0:
@@ -35,10 +33,15 @@ while len(stati)<9:
             stato=0
         stati.append(1)
     stati.append(stato)
-for i, stato in enumerate(stati[1::]):
-    rami.append(Ramo(screen, stato, 9-i))
+for i, stato in enumerate(stati):
+    rami.append(Ramo(screen, stato, 7-i))
 
-
+for ramo in rami:
+    print(ramo.stato, end=" ")
+print()
+for ramo in rami:
+    print(ramo.n, end=" ")
+print()
 pygame.display.set_caption("Lumberjack Quandale Deangle")
 
 fps=60
@@ -58,35 +61,56 @@ while True:
             if event.key==pygame.K_RIGHT:
                 if boscaiolo.pos==0:
                     boscaiolo.pos=2
+                for i in range(9):
+                    rami[i]=rami[i+1]
+                    rami[i].y+=50
+                    rami[i].n+=1
+                n=rand(supporto)
+                if n<7:
+                    rami[9]=Ramo(screen, rami[8].stato, -2)
                 else:
-                    if len(rami)<9:
-                        for i in range(len(rami)-1):
-                            rami[i]=rami[i+1]
-                            rami[i].y+=50
-                        n=rand(supporto)
-                        if n<8:
-                            rami.append(Ramo(screen, rami[-1].stato, len(rami)))
-                        else
+                    if rami[8].stato==0:
+                        rami[9]=Ramo(screen, 2, -2)
+                    else:
+                        rami[9]=Ramo(screen, 0, -2)
+                        
+                    rami[8]=Ramo(screen, 1, -1)
+                
+
 
             if event.key==pygame.K_LEFT:
                 if boscaiolo.pos==2:
                     boscaiolo.pos=0
                 else:
-                    if len(rami)<9:
-                        for i in range(len(rami)-1):
-                            rami[i]=rami[i+1]
-                            rami[i].y+=50
-                        n=rand(supporto)
-                        if n<8:
-                            rami.append(Ramo(screen, rami[-1].stato, len(rami)))
+                    for i in range(9):
+                        rami[i]=rami[i+1]
+                        rami[i].y+=50
+                        rami[i].n+=1
+                    n=rand(supporto)
+                if n<7:
+                    rami[9]=Ramo(screen, rami[8].stato, -2)
+                else:
+                    if rami[8].stato==0:
+                        rami[9]=Ramo(screen, 2, -2)
+                    else:
+                        rami[9]=Ramo(screen, 0, -2)
+                        
+                    rami[8]=Ramo(screen, 1, -1)
+                
+            for ramo in rami:
+                print(ramo.stato, end=" ")
+            print()
+            for ramo in rami:
+                print(ramo.n, end=" ")
+            print()
     boscaiolo.draw()
-    if rami[0]!=None:
-        if rami[0].stato==boscaiolo.pos:
-            perso=True
-    
-    for ramo in rami:
-        if ramo!=None:
-            ramo.draw()
+    if rami[0].stato==boscaiolo.pos:
+        perso=True
+        
+    for ramo in rami[0:9:]:
+        ramo.draw()
+    if perso:
+        screen.fill("Yellow")
     clock.tick(fps)
     pygame.display.flip()
     
