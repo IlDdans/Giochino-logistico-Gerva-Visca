e00dd13b6dd6ebc0029e6e3c738f82b1b74417df
er cinese visca <viscardijacopo6@gmail.com>
2024-05-27 12:06:10 +0200

    abbiamo skibiditoilettato in classe con lei presente

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ WIDTH, HEIGHT = 600, 600
 screen=pygame.display.set_mode((WIDTH, HEIGHT))
 perso=False
 sfondo=pygame.image.load("foresta.png")
+font=pygame.font.Font(None,30)
 
 sconfitta=pygame.image.load("perso.png")
 sconfitta=pygame.transform.scale(sconfitta,(600,600))
@@ -16,10 +17,42 @@ tronco=pygame.image.load("tronco.jpg").convert_alpha()
 tronco=pygame.transform.scale(tronco, (100, 550))
 tronco_rect=tronco.get_rect(midtop=(WIDTH/2, 0))
 
+ceppetto=pygame.image.load("ceppetto.png")
+ceppetto=pygame.transform.scale(ceppetto, (52, 52))
+
+punteggio=0
+
+lista_caricamento=[]
+carcica1=pygame.image.load("22.png")
+carcica1=pygame.transform.scale(carcica1,(carcica1.get_width()*0.1,carcica1.get_height()*0.1))
+carcica2=pygame.image.load("54.png")
+carcica2=pygame.transform.scale(carcica2,(carcica2.get_width()*0.1,carcica2.get_height()*0.1))
+carcica3=pygame.image.load("77.png")
+carcica3=pygame.transform.scale(carcica3,(carcica3.get_width()*0.1,carcica3.get_height()*0.1))
+carcica4=pygame.image.load("100.png")
+carcica4=pygame.transform.scale(carcica4,(carcica4.get_width()*0.11,carcica4.get_height()*0.11))
+
+lista_caricamento.append(carcica1)
+lista_caricamento.append(carcica2)
+lista_caricamento.append(carcica3)
+lista_caricamento.append(carcica4)
 
 boscaiolo=Boscaiolo(screen)
 
+sfondo1=pygame.image.load("sfondo1.png")
+sfond1=pygame.transform.scale(sfondo1,(600,600))
 
+def caricamento():
+    stato=True
+    i=0
+    while stato:
+        for i in range(len(lista_caricamento)):
+            screen.blit(sfondo1,(0,0))
+            screen.blit(lista_caricamento[i],(250,550))
+            pygame.display.update()
+            pygame.time.delay(1200)
+            if i==3:
+                stato=False
 
 supporto=[i for i in range(0,9)]
 stati=[]
@@ -28,7 +61,7 @@ stati.append(1)
 stati.append(rand([0,2]))
 while len(stati)<10:
     n=rand(supporto)
-    if n<7:
+    if n<6:
         stato=stati[-1]
     else:
         if stati[-1]==0:
@@ -38,7 +71,7 @@ while len(stati)<10:
         stati.append(1)
     stati.append(stato)
 for i, stato in enumerate(stati):
-    rami.append(Ramo(screen, stato, 7-i))
+    rami.append(Ramo(screen, stato, 6-i))
 
 for ramo in rami:
     print(ramo.stato, end=" ")
@@ -54,17 +87,19 @@ fps=60
 clock=pygame.time.Clock()
 font=pygame.font.Font(None, 50)
 
-
+caricamento()
 while True:
     
     screen.blit(sfondo, (0,0))
     screen.blit(tronco, tronco_rect)
+    screen.blit(ceppetto, (525, 540))
     for event in pygame.event.get():
         if event.type==pygame.QUIT:     
             pygame.quit()           
             sys.exit()
         if event.type==pygame.KEYDOWN:
             if event.key==pygame.K_RIGHT:
+                punteggio+=10
                 if boscaiolo.pos==0:
                     boscaiolo.pos=2
                 for i in range(9):
@@ -78,7 +113,7 @@ while True:
                     if rami[8].stato==0:
                         rami[9]=Ramo(screen, 2, -2)
                     else:
-                        rami[9]=Ramo(screen, 0, -2)
+                        rami[9]=Ramo(screen, 0, -2) 
                         
                     rami[8]=Ramo(screen, 1, -1)
                 
@@ -86,6 +121,7 @@ while True:
 
 
             if event.key==pygame.K_LEFT:
+                punteggio+=10
                 if boscaiolo.pos==2:
                     boscaiolo.pos=0
                 else:
@@ -109,6 +145,10 @@ while True:
             for ramo in rami:
                 print(ramo.n, end=" ")
             print()
+    fontino=font.render(f"{punteggio}",True,(250,250,250))
+    font_rect=fontino.get_rect()
+    font_rect.center=(480,566)
+    screen.blit(fontino,font_rect)
     boscaiolo.draw()
     if rami[0].stato==boscaiolo.pos:
         perso=True
